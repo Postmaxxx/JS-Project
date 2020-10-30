7ef80e654a61cf2cda36310fa1d8bf6c4bd0d17c
Postmaxxx <71179917+Postmaxxx@users.noreply.github.com>
2020-10-31 03:44:07 +0800

    Full version

--- src/source.js
+++ src/source.js
@@ -4,21 +4,21 @@ let objects = [
         area: "81 m2",
         time: "3.5 months",
         cost: "Upon request",
-        image: "linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(../images/page1-image1.png)" //linear-gradient сделано для дальнейшего украшательста
+        image: "linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(./images/page1-image1.png)" //linear-gradient сделано для дальнейшего украшательста
     },
     {
         city: "Sochi</br>Thieves",
         area: "105 m2",
         time: "4 months",
         cost: "Upon request",
-        image: "linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(../images/page1-image2.png)"
+        image: "linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(./images/page1-image2.png)"
     },
     {
         city: "Rostov-on-Don</br>Patriotic",
         area: "93 m2",
         time: "3 months",
         cost: "Upon request",
-        image: "linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(../images/page1-image3.png)"
+        image: "linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(./images/page1-image3.png)"
     }
 ];
 
